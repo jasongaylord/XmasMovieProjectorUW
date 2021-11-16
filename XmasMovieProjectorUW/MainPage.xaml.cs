7e2dc60bb44b629a060093f18bcd95b2e96e7841
Jason Gaylord <jason@jasongaylord.com>
2021-11-16 17:09:31 -0500

    Update the app to play videos based on the status file being read in from the config.txt file.

--- XmasMovieProjectorUW/MainPage.xaml.cs
+++ XmasMovieProjectorUW/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -7,6 +8,8 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Core;
 using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.Search;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -25,15 +28,29 @@ namespace XmasMovieProjectorUW
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public Dictionary<string, string> ConfigValues;
+        public Dictionary<string, string> SongStatus;
+        public DateTime FileLastChecked;
+        public DateTime FileNextCheck;
+        public System.Timers.Timer Timer1;
+        public MediaPlayer mediaPlayer;
+        public bool isPlaying;
+       
         public MainPage()
         {
             this.InitializeComponent();
 
-            var mediaPlayer = new MediaPlayer();
-            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/VID00006.MP4"));
-            mediaPlayer.IsLoopingEnabled = true;
-            mediaPlayer.Play();
+            mediaPlayer = new MediaPlayer
+            {
+                IsLoopingEnabled = true,
+                Volume = 0
+            };
             _mediaPlayerElement.SetMediaPlayer(mediaPlayer);
+            isPlaying = false;
+
+            LoadConfig();
+            EnableFileWatcher();
+            PlayVideo();
         }
 
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
@@ -50,7 +67,99 @@ namespace XmasMovieProjectorUW
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
+            Timer1.Stop();
+            Timer1.Dispose();
             Application.Current.Exit();
+        }
+
+        private void LoadConfig()
+        {
+            ConfigValues = new Dictionary<string, string>();
+            var config = File.ReadAllText("Assets/Config.txt");
+            var configLines = config.Split("\n");
+
+            foreach (var item in from line in configLines
+                                 let item = line.Split("|")
+                                 select item)
+                ConfigValues.Add(item[0], item[1].Replace("\r", ""));
+        }
+
+        private void EnableFileWatcher()
+        {
+            FileLastChecked = DateTime.Now;
+            SongStatus = new Dictionary<string, string>();
+
+            Timer1 = new System.Timers.Timer();
+            Timer1.Elapsed += Timer1_Elapsed;
+
+            CheckFile();
+        }
+
+        private void Timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Timer1.Stop();
+            CheckFile();
+        }
+
+        //private async Task ReadStatusContents
+
+        private void CheckFile()
+        {
+            var folder = StorageFolder.GetFolderFromPathAsync(ConfigValues["StatusFolder"]).GetAwaiter().GetResult();
+            var file = folder.GetFileAsync(ConfigValues["StatusFile"]).GetAwaiter().GetResult();
+            var statusFile = FileIO.ReadTextAsync(file).GetAwaiter().GetResult();
+
+            // Load the Status File
+            //var statusFile = File.ReadAllText(ConfigValues["StatusFile"]);
+            var statusFileContents = statusFile.Split("\n");
+
+            SongStatus.Clear();
+            foreach (var item in from line in statusFileContents
+                                 let item = line.Split("|")
+                                 select item)
+                SongStatus.Add(item[0], item[1].Replace("\r", ""));
+
+            int interval = 10000;
+            var intSuccess = int.TryParse(SongStatus["Interval"], out interval);
+            if (!intSuccess)
+                interval = 10000;
+
+            FileNextCheck = DateTime.Now.AddMilliseconds(interval);
+
+            Timer1.Interval = interval;
+            Timer1.Start();
+
+            if (SongStatus["SequenceType"] == "2" && isPlaying)
+            {
+                StopVideo();
+            } else if (SongStatus["SequenceType"] == "1" && !isPlaying)
+            {
+                PlayVideo();
+            } else if (SongStatus["SequenceType"] == "0" && isPlaying)
+            {
+                StopVideo();
+            }
+
+            //// Load current day showtimes
+            //var currentDay = FileLastChecked.ToString("ddd");
+            //var showtimeSetting = ConfigValues[currentDay];
+            //var showtimes = showtimeSetting.Split(",");
+        }
+
+        private void PlayVideo()
+        {
+            mediaPlayer.Pause();
+            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/" + ConfigValues["ActionVideo"]));      
+            mediaPlayer.Play();
+            isPlaying = true;
+        }
+
+        private void StopVideo()
+        {
+            mediaPlayer.Pause();
+            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/" + ConfigValues["PausedVideo"]));
+            mediaPlayer.Play();
+            isPlaying = false;
         }
     }
 }
