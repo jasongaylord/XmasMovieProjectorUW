6ace18c0cec068e2db5aac9c8d8ed35de6ea97a4
Jason Gaylord <jason@jasongaylord.com>
2021-11-16 20:26:34 -0500

    Other minor updates for the projector

--- XmasMovieProjectorUW/MainPage.xaml.cs
+++ XmasMovieProjectorUW/MainPage.xaml.cs
@@ -135,14 +135,53 @@ namespace XmasMovieProjectorUW
                 StopVideo();
             }
 
-            //// Load current day showtimes
-            //var currentDay = FileLastChecked.ToString("ddd");
-            //var showtimeSetting = ConfigValues[currentDay];
-            //var showtimes = showtimeSetting.Split(",");
+            if (SongStatus["SequenceType"] == "2")
+            {
+                _nextShowTime.Text = "";
+                _scoreboard.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                // Load current day showtimes
+                var currentDay = FileLastChecked.ToString("ddd");
+                var showtimeSetting = ConfigValues[currentDay];
+                var showtimes = showtimeSetting.Split(",");
+
+                // Iterate through showtimes to find the next showtime
+                var currentTimeTicks = DateTime.Now.Ticks;
+                var currentDate = DateTime.Now.ToShortDateString();
+                var nextShowtime = new DateTime();
+                var foundNextShow = false;
+                foreach (var showtime in showtimes)
+                {
+                    var showtimeDate = DateTime.Parse(currentDate + " " + showtime);
+                    if (showtimeDate.Ticks > currentTimeTicks)
+                    {
+                        nextShowtime = showtimeDate;
+                        foundNextShow = true;
+                    }
+
+                    if (foundNextShow)
+                        break;
+                }
+
+                // Set the Next ShowTime Text
+                var nextShowTimeText = "Tomorrow";
+
+                if (foundNextShow)
+                {
+                    var spanUntilNextShow = nextShowtime - DateTime.Now;
+                    nextShowTimeText = spanUntilNextShow.Minutes + " Minutes";
+                }
+
+                _nextShowTime.Text = nextShowTimeText;
+                _scoreboard.Visibility = Visibility.Visible;
+            }
         }
 
         private void PlayVideo()
         {
+            _scoreboard.Visibility = Visibility.Visible;
             _mediaPlayerElement.Visibility = Visibility.Visible;
             mediaPlayer.Play();
             isPlaying = true;
@@ -150,6 +189,7 @@ namespace XmasMovieProjectorUW
 
         private void StopVideo()
         {
+            _scoreboard.Visibility = Visibility.Collapsed;
             _mediaPlayerElement.Visibility = Visibility.Collapsed;
             mediaPlayer.Pause();
             isPlaying = false;
